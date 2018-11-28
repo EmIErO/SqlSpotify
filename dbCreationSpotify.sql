313870695c741d44a24708b93676f6efc9650469
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-11-28 11:33:10 +0100

    Correct misspell

--- dbCreationSpotify.sql
+++ dbCreationSpotify.sql
@@ -13,30 +13,32 @@ DROP TABLE IF EXISTS band CASCADE;
 CREATE TABLE band
 (id_band SERIAL PRIMARY KEY,
 name VARCHAR(50),
+country VARCHAR(50),
 start_date DATE,
-end_date DATE,
-country VARCHAR(50) NOT NULL
+end_date DATE
 );
 
 DROP TABLE IF EXISTS artist_band CASCADE;
 CREATE TABLE artist_band
 (id_artist INTEGER REFERENCES artist (id_artist),
-id_band INTEGER REFERENCES band (id_band)
+id_band INTEGER REFERENCES band (id_band),
+join_date DATE
 );
 
 DROP TABLE IF EXISTS album CASCADE;
 CREATE TABLE album
 (id_album SERIAL PRIMARY KEY,
 name VARCHAR(50),
-genere VARCHAR(50),
-release_date DATE
+genre VARCHAR(50),
+release_date DATE,
+id_band INTEGER REFERENCES band (id_band)
 );
 
 DROP TABLE IF EXISTS song CASCADE;
 CREATE TABLE song
 (id_song SERIAL PRIMARY KEY,
 title VARCHAR(50),
-lenght TIME
+length TIME
 );
 
 DROP TABLE IF EXISTS song_album CASCADE;
@@ -53,7 +55,7 @@ login VARCHAR(50),
 password VARCHAR(50),
 firstname VARCHAR(50),
 surname VARCHAR(50),
-gender VARCHAR(1),
+gender VARCHAR(6),
 email VARCHAR(50) NOT NULL
 );
 
@@ -74,7 +76,6 @@ DROP TABLE IF EXISTS subscription CASCADE;
 CREATE TABLE subscription
 (id_subscription SERIAL PRIMARY KEY,
 name VARCHAR(50),
-password VARCHAR(50),
 days INTEGER,
 cost INTEGER
 );
