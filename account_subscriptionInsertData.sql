9bac5a74d41b0a035a9c87b333724cde1e068928
EmIErO <mira.bryja@gmail.com>
2018-11-29 22:56:31 +0100

    Add view of all invalid subscriptions sorted descendingly by end date

--- account_subscriptionInsertData.sql
+++ account_subscriptionInsertData.sql
@@ -1,9 +1,9 @@
 DELETE FROM account_subscription;
 
 INSERT INTO account_subscription (id_account, id_subscription, start_date)
-    SELECT id_account,
-           random_subscription(),
-           random_date('2018-01-01', '2018-11-29')
+SELECT id_account,
+       random_subscription(),
+       random_date('2018-01-01', '2018-11-29')
 FROM account
 WHERE id_account % 7 > 3 AND firstname LIKE '%a%';
 
@@ -11,33 +11,42 @@ WHERE id_account % 7 > 3 AND firstname LIKE '%a%';
 
 
 CREATE OR REPLACE FUNCTION random_date (fromDate DATE, toDate DATE) RETURNS DATE  AS $$
-
 DECLARE
  days_between INT;
  days_rand INT;
 
 BEGIN
-
  SELECT $2 - $1 INTO days_between;
  SELECT cast(random() * days_between as INT) INTO days_rand;
-
  RETURN $1 + days_rand;
-
 END;
 $$ LANGUAGE 'plpgsql';
 
 
 
-CREATE  FUNCTION random_subscription () RETURNS INT AS $$
-
+CREATE OR REPLACE FUNCTION random_subscription () RETURNS INT AS $$
 DECLARE
  id INT;
 
  BEGIN
-
  SELECT id_subscription FROM subscription ORDER BY random() LIMIT 1 INTO id;
  RETURN id;
-
  END;
 $$ LANGUAGE  'plpgsql';
+
+CREATE VIEW invalidSubscription AS
+    SELECT a.firstname || ' ' || a.surname AS fullName,
+           asub.start_date AS startDate,
+           asub.start_date + s.days AS endDate,
+           s.name AS subsriptionType
+        FROM account_subscription asub
+             INNER JOIN account a ON  asub.id_account = a.id_account
+             INNER JOIN subscription s on asub.id_subscription = s.id_subscription
+WHERE asub.start_date + s.days < current_date
+ORDER BY endDate DESC;
+
+
+SELECT * FROM invalidSubscription;
+DROP VIEW invalidSubscription;
+
 
